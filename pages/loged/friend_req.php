8ecd6cc4f539ca65a8f8ccf27b21cfb661120941
becsei szabolcs <becsei.szabi@gmail.com>
2024-03-01 15:28:51 +0100

    add more funtion

--- pages/loged/friend_req.php
+++ pages/loged/friend_req.php
@@ -5,9 +5,16 @@
     $fustrid = $_POST["profile"];
     $fuid = mysqli_fetch_assoc(mysqli_query($dbase,"select uid from users where ustrid='$fustrid'"))["uid"];
 
-    $friend = mysqli_fetch_row(mysqli_query($dbase,"select * from friends where ($_SESSION[uid] = uid and fuid = $fuid) or ($fuid = uid and fuid = $_SESSION[uid])"));
+    $friend = mysqli_fetch_assoc(mysqli_query($dbase,"select * from friends where ($_SESSION[uid] = uid and fuid = $fuid) or ($fuid = uid and fuid = $_SESSION[uid])"));
 
-    if(!$friend){
+    if($friend){
+        if($friend["fstat"] !='A'){
+            if($friend["uid"] == $_SESSION["uid"]) mysqli_query($dbase,"DELETE FROM friends WHERE fid = $friend[uid]");
+            else                                   mysqli_query($dbase,"UPDATE friends set fstat = 'A' WHERE fid = $friend[uid]");
+        }
+
+
+    } else{
         mysqli_query($dbase,"
             INSERT INTO  friends  ( fid ,  uid ,  fuid ,  fdate ,  fstat , fip) 
             VALUES (NULL, $_SESSION[uid], $fuid, current_timestamp(),'D', '$_SERVER[REMOTE_ADDR]')
@@ -16,4 +23,4 @@
     mysqli_close($dbase);
 
 
-?>
+
