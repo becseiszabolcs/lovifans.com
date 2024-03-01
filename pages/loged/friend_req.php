f47a9012ffd95bc049064e18a09e6770c5a6d227
becsei szabolcs <becsei.szabi@gmail.com>
2024-03-01 15:00:01 +0100

    friend_req fix

--- pages/loged/friend_req.php
+++ pages/loged/friend_req.php
@@ -5,11 +5,15 @@
     $fustrid = $_POST["profile"];
     $fuid = mysqli_fetch_assoc(mysqli_query($dbase,"select uid from users where ustrid='$fustrid'"))["uid"];
 
-    
-    mysqli_query($dbase,"
-        INSERT INTO  friends  ( fid ,  uid ,  fuid ,  fdate ,  fstat , fip) 
-        VALUES (NULL, $_SESSION[uid], $fuid, current_timestamp(),'D', '$_SERVER[REMOTE_ADDR]')
-    ");
-    
+    $friend = mysqli_fetch_row(mysqli_query($dbase,"select * from friends where ($_SESSION[uid] = uid and fuid = $fuid) or ($fuid = uid and fuid = $_SESSION[uid])"));
+
+    if(!$friend){
+        mysqli_query($dbase,"
+            INSERT INTO  friends  ( fid ,  uid ,  fuid ,  fdate ,  fstat , fip) 
+            VALUES (NULL, $_SESSION[uid], $fuid, current_timestamp(),'D', '$_SERVER[REMOTE_ADDR]')
+        ");
+    }
+    mysqli_close($dbase);
+
 
 ?>
