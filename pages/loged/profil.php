11f7a7c3fb1b850801acf7ebc6243a5005a377b3
becseiszabolcs <becsei.szabi@gmail.com>
2024-03-18 18:04:40 +0100

    profil_page

--- pages/loged/profil.php
+++ pages/loged/profil.php
@@ -4,9 +4,44 @@ if(!isset($_SESSION["uid"])) header("Location: $_SESSION[R1]");
 ?>
 
 <div class="prof sites">
-    
-    
+    <div class="bar">
+
+    </div>
+    <div class="sections">
+        <img src="" alt="" id="bcimg">
+        <div class="header">
+            <img src="" alt="profil image" id="profimg">
+            <h1 class="profilname">
+                Profil Name
+            </h1>
+        </div>
+        <div class="posts">
+            <div class="post">
+                <div class="header">
+                    <img src="" alt="profil image" id="profimg">
+                    <h1 class="profilname_post">
+                        Profil Name
+                    </h1>
+                </div>
+                <div>
+                    <p class="postmess">
+                        Hello, World!
+                    </p>
+                    <img src="" alt="" class="postimg">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+  
+
+<script>
+    //profilload();
+    //setInterval(profilload,5000);
+</script>
 </div>
+
 
 
 
