e48eb581ba987fdd4195f174681ac65f7de48966
becsei szabolcs <becsei.szabi@gmail.com>
2024-04-05 15:59:53 +0200

    remove print

--- pages/loged/post.php
+++ pages/loged/post.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include("$_SESSION[priv]/connect.php");
-print_r($_FILES);
+
 if(isset($_FILES["files"])){
     $len = count($_FILES["files"]["name"]);
     for($i=0;$i<$len;$i+=1) file_upload($dbase,$_FILES["files"],$i);
